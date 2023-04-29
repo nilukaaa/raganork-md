39f2de7cf2343baf9598de529b479d12c4dd12a9
nilukaaa <130591982+nilukaaa@users.noreply.github.com>
2023-04-29 14:25:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -47,8 +47,8 @@ module.exports = {
     ANTILINK_WARN: process.env.ANTILINK_WARN || '',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
-        API_KEY: process.env.HEROKU_API_KEY || '',
-        APP_NAME: process.env.HEROKU_APP_NAME || ''
+        API_KEY: process.env.HEROKU_API_KEY || '4d2ce946-ed56-4510-b2de-deeb1d040607',
+        APP_NAME: process.env.HEROKU_APP_NAME || 'rrkkk'
     },
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
